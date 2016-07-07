8e02018e2976e3f95b87acec9bf675975671f24a
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-07-07 10:02:18 -0300

    add: webpack config

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,58 @@
+var path = require('path');
+var webpack = require('webpack');
+
+// App files location
+var PATHS = {
+  app: path.resolve(__dirname, '../source/js'),
+  build: path.resolve(__dirname, '../dist')
+};
+
+var plugins = [
+  // Avoid publishing files when compilation fails
+  new webpack.NoErrorsPlugin(),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('development'),
+    __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+  }),
+  new webpack.optimize.OccurenceOrderPlugin()
+];
+
+module.exports = {
+  env : process.env.NODE_ENV,
+  entry: {
+    app: path.resolve(PATHS.app, 'main.js')
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'js/scripts.min.js',
+    publicPath: '/'
+  },
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  resolve: {
+    // We can now require('file') instead of require('file.jsx')
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: plugins,
+  devServer: {
+    contentBase: path.resolve(__dirname, PATHS.build),
+    port: 8000,
+    historyApiFallback: true
+  },
+  devtool: 'eval'
+};
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var webpack = require('webpack');
+
+// App files location
+var PATHS = {
+  app: path.resolve(__dirname, '../source/js'),
+  build: path.resolve(__dirname, '../dist')
+};
+
+var plugins = [
+  new webpack.NoErrorsPlugin(),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+  }),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    comments: false,
+    sourceMap: false
+  })
+];
+
+module.exports = {
+  env : process.env.NODE_ENV,
+  entry: {
+    app: path.resolve(PATHS.app, 'main.js')
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'js/scripts.min.js',
+    publicPath: '/'
+  },
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  resolve: {
+    // We can now require('file') instead of require('file.jsx')
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: plugins,
+  devtool: 'eval'
+};
